649a2dd4acb21934ecca4206e9a6e4e470f53378
Jose Enrique Ruiz Navarro <joseenriqueruiznavarro@gmail.com>
2017-08-27 20:42:13 +0200

    sed every env var

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,9 @@ FROM ubuntu:16.04
 MAINTAINER Jose Enrique Ruiz Navarro, quique.ruiz@podgroup.com
 
 ENV KONG_VERSION 0.11.0
+
+ENV KONG_CONFIG /etc/kong/kong.conf
+
 ENV KONG_SHA256 34cfd44f61a4da5d39ad7b59bad7b4790451065ff8c8c3d000b6258ab6961949
 
 RUN apt-get update && apt-get install curl perl -y && \
@@ -9,8 +12,14 @@ curl -Lso /tmp/kong.deb \
      "https://bintray.com/kong/kong-community-edition-deb/download_file?file_path=dists/kong-community-edition-0.11.0.xenial.all.deb"  && \
      dpkg -i /tmp/kong.deb
 
+RUN mv /etc/kong/kong.conf.default ${KONG_CONFIG}
+
+RUN apt-get install python-pip -y && pip install cqlsh
+RUN apt-get remove --purge python-pip -y
 
 ADD scripts/docker-entrypoint.sh /docker-entrypoint.sh
 ENTRYPOINT ["./docker-entrypoint.sh"]
 
 EXPOSE 8000 8443 8001 8444
+
+CMD ["bash"]
--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,3 +1,64 @@
 #!/bin/bash
-echo "entra"
+IP=`hostname --ip-address | cut -f 1 -d ' '`
+ALL='0.0.0.0'
+
+if [ -z "$KONG_DATABASE" ]; then
+        echo "PostgreSQL by default"
+else
+        sed -i -e "s/^\#database = postgres/database = $KONG_DATABASE/" $KONG_CONFIG
+fi
+
+
+if [ -z "$KONG_CASSANDRA_CONTACTPOINTS" ]; then
+        echo "No contact points"
+else
+        sed -i -e "s/^\#cassandra_contact_points = 127.0.0.1/cassandra_contact_points = $KONG_CASSANDRA_CONTACTPOINTS/" $KONG_CONFIG
+fi
+
+if [ -z "$KONG_CASSANDRA_USERNAME" ]; then
+        echo "No Username for cassandra"
+else
+        sed -i -e "s/^\#cassandra_username = kong/cassandra_username = $KONG_CASSANDRA_USERNAME/" $KONG_CONFIG
+fi
+
+if [ -z "$KONG_CASSANDRA_PASSWORD" ]; then
+        echo "No password for cassandra user"
+else
+        sed -i -e "s/^\#cassandra_password =/cassandra_password = $KONG_CASSANDRA_PASSWORD/" $KONG_CONFIG
+fi
+
+if [ -z "$KONG_CASSANDRA_STRATEGY" ]; then
+        echo "Simple Strategy by default"
+else
+        sed -i -e "s/^\#cassandra_repl_strategy = SimpleStrategy/cassandra_repl_strategy = $KONG_CASSANDRA_STRATEGY/" $KONG_CONFIG
+fi
+
+if [ -z "$KONG_CASSANDRA_FACTOR" ]; then
+        echo "Simple Strategy by default"
+else
+        sed -i -e "s/^\#cassandra_repl_factor = 1/cassandra_repl_factor = $KONG_CASSANDRA_FACTOR/" $KONG_CONFIG
+fi
+
+
+if [ -z "$KONG_CASSANDRA_PROPAGATION" ]; then
+        echo "Propagation is 0"
+else
+        sed -i -e "s/^\#db_update_propagation = 0/db_update_propagation = $KONG_CASSANDRA_PROPAGATION/" $KONG_CONFIG
+fi
+
+if [ -z "$KONG_CASSANDRA_FREQUENCY" ]; then
+        echo "Frequency is 0"
+else
+        sed -i -e "s/^\#db_update_frequency = 5/db_update_frequency = $KONG_CASSANDRA_FREQUENCY/" $KONG_CONFIG
+fi
+
+if [ -z "$KONG_DAEMON" ]; then
+        echo "Daemon is on"
+else
+  if [[ $KONG_DAEMON = "off" ]]; then
+    sed -i -e "s/^\#nginx_daemon = on/nginx_daemon = $KONG_DAEMON/" $KONG_CONFIG
+  fi
+fi
+
+
 exec "$@"
